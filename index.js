8a451cedffbfe5b69f93c6842a3d6600c371219f
Johnny <jaani.matharu@gmail.com>
2021-05-17 01:41:54 -0400

    code is working, minor fixes needed

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const questions = () => {
           console.log(inputDataOne,inputDataTwo,inputDataThree,"addRoles function check"); 
           return this.connection.query(
             //  name, salary, and department for the role
-            'INSERT INTO Roles (job_title) VALUE(?);', inputDataOne 
+            'INSERT INTO Roles (job_title, department_name, salary) VALUES(?,?,?);', [inputDataOne, inputDataTwo, inputDataThree] 
             );
             };
 
@@ -264,12 +264,11 @@ WHEN I choose to view all employees
 THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 WHEN I choose to add a department
 THEN I am prompted to enter the name of the department and that department is added to the database
-
-HERE
-
-
 WHEN I choose to add a role
 THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+
+HERE
 
 WHEN I choose to add an employee
 THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
